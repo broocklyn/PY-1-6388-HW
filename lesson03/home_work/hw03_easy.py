a0f48e8bbfae5b883bbfed59a6de22385c81efa3
Pavel Uchuskin <d.broocklyn@gmail.com>
2019-04-10 23:15:05 +0300

    добавлено решение второй задачи

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -30,8 +30,13 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
-
+	num = str(ticket_number)
+	if len(num) == 6:
+		sum1 = int(num[0]) + int(num[1]) + int(num[2])
+		sum2 = int(num[3]) + int(num[4]) + int(num[5])
+		if sum1 == sum2:
+			return 'счастливый билетик'
+	else: return 'обычный билет'
 
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
